51ccdec4122771439e3ed57711341c9c0b8d13d8
Nazmican Dikmen <ncandikmen@outlook.com>
2022-02-26 23:17:10 +0300

    Quotation

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 mongoose.connect(process.env.DATABASE_URI || "mongodb://localhost/api", {
     useNewUrlParser: true,
